5514319d30f189ab8b5c19d558ac916b806bd535
PanhaGit <152314378+PanhaGit@users.noreply.github.com>
2024-04-24 00:38:12 +0700

    s

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="w-full bg-gray-100">
+    <div className="w-full bg-gray-100 mt-7">
       <footer className="bg-gray-100 py-7 w-11/12 m-auto">
         <h4 className="text-center">
           <a href="/">copyright &copy; {currentYear} by: Panha Tho</a>
